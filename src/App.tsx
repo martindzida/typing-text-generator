2d7ed335ce00bbab4da70d5d2c3110e92ceb0dc3
martindzida <martin.d14@email.cz>
2023-01-03 17:40:22 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='w-full min-h-screen flex flex-col justify-center items-center bg-slate-600 text-amber-400'>
+      <h1 className='text-6xl font-mono'>Typing Text Generator</h1>
+    </div>
+  )
+}
+
+export default App
