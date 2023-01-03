304143fe4e1c4bee7743df2af4de1a9b7b33c752
martindzida <martin.d14@email.cz>
2023-01-03 21:19:27 +0100

    app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import SettingsForm from './components/SettingsForm'
+import DisplayText from './components/DisplayText'
 
 function App() {
+  const [curText, setCurText] = useState<string | null>(null)
+
   return (
     <div className='w-full min-h-screen flex flex-col justify-center items-center bg-slate-700 font-mono'>
       <h1 className='text-amber-400 text-6xl p-16'>Typing Text Generator</h1>
-      <SettingsForm />
+      {curText ? (
+        <DisplayText text={curText} genNewText={setCurText} />
+      ) : (
+        <SettingsForm getGeneratedText={setCurText} />
+      )}
     </div>
   )
 }
