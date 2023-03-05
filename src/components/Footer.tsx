e6fba2c277236ebd2a204a852e34659df66a176b
martindzida <martin.d14@email.cz>
2023-03-05 13:35:09 +0100

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className='w-full flex justify-center text-amber-400 text-center border-t border-amber-400 p-8 mt-16'>
+      <span className='pr-4'>Inspired by this wonderful typing site:</span>
+      <a href='https://monkeytype.com/'>monkeytype.com</a>
+    </div>
+  )
+}
+
+export default Footer
